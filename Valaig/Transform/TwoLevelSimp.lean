e1703fa5b8a2ed36c4aea3c5fb9929ad56e99bfc
George Rennie <georgerennie@gmail.com>
2026-06-10 17:14:22 +0100

    Use caching in Transform.TwoLevelSimp
    * This proof is incredibly slow but we can revisit this later

--- Valaig/Transform/TwoLevelSimp.lean
+++ Valaig/Transform/TwoLevelSimp.lean
@@ -14,46 +14,48 @@ to a single literal, it is replaced with an and-gate with two inputs the same.
 
 namespace twoLevelSimp
 
-@[inline]
-def resolveInputs (aig : WFAig) (var : Var) (valid : var.validIn aig := by grind) : Option (Lit × Lit × Lit × Lit × Lit × Lit) :=
-  match _ : aig[var] with
-  | .and l r =>
-    match _ : aig[l.var], _ : aig[r.var] with
-    | .and l0 l1, .and r0 r1 =>
-      let (l, l0, l1) :=
-        if l0 = l1 then
-          match aig[l0.var] with
-          | .and l00 l01 => (l.invert l0.inverted, l00, l01)
-          | _ => (l, l0, l1)
-        else
-          (l, l0, l1)
-
-      let (r, r0, r1) :=
-        if r0 = r1 then
-          match aig[r0.var] with
-          | .and r00 r01 => (r.invert r0.inverted, r00, r01)
-          | _ => (r, r0, r1)
-        else
-          (r, r0, r1)
-
-      some (l, r, l0, l1, r0, r1)
-    | _, _ => none
-  | _ => none
-
 @[always_inline]
-private def walker (old : WFAig) : old.ForwardsWalker WFAig where
-  motive aig size le := ∀ {var : Var}, var.validIn old → var.validIn aig
+private def walker (old : WFAig) : old.CachingForwardsWalker WFAig Lit where
+  stateMotive aig size le := ∀ {var : Var}, var.validIn old → var.validIn aig
+  cacheMotive aig idx le sm var lt lit := lit.var.idx < idx
 
-  step var aig valid sm := Id.run do
-    let some (lhs, rhs, l0, l1, r0, r1) := resolveInputs aig var | return aig
-    match TwoLevelSimp.simplifyAnd lhs rhs l0 l1 r0 r1 with
-    | .lit l => return aig.rewriteAnd! var l l |>.get! |>.toWF sorry
-    | .and l r => return aig.rewriteAnd! var l r |>.get! |>.toWF sorry
+  step var aig cache valid size sm cm  := Id.run do
+    let (eq:=_) .and lhs rhs := aig[var] | return (aig, var)
 
-  motiveStep := sorry
+    -- Map through cache to make sure we have optimized versions
+    let lhs := cache.mapLit lhs
+    let rhs := cache.mapLit rhs
+
+    match _ : aig[lhs.var], _ : aig[rhs.var] with
+    | .and l0 l1, .and r0 r1 =>
+      -- We don't map l0/l1/r0/r1 as they should have been updated by the cache already
+      match  _ : TwoLevelSimp.simplifyAnd lhs rhs l0 l1 r0 r1 with
+      | .lit l => (aig.rewriteAnd var l l, l)
+      | .and l r => (aig.rewriteAnd var l r, var)
+    | _, _ => (aig.rewriteAnd var lhs rhs, var)
+
+  stepState := by
+    intros
+    simp only
+    (repeat' split)
+    · rw [Id.run, WFAig.raw_rewriteAnd, var_validIn, nodes_rewriteAnd] <;> grind
+    · rw [Id.run, WFAig.raw_rewriteAnd, var_validIn, nodes_rewriteAnd] <;> grind
+    · rw [Id.run, WFAig.raw_rewriteAnd, var_validIn, nodes_rewriteAnd] <;> grind
+    · grind
+
+  stepCache := by grind
+  stepCacheNew := by
+    intros
+    simp only
+    (repeat' split)
+    · rw [Id.run]; grind
+    · rw [Id.run]; grind
+    · rw [Id.run]; grind
+    · grind
+
 end twoLevelSimp
 
 def twoLevelSimp (aig : WFAig) : WFAig :=
-  (twoLevelSimp.walker aig).walk aig (by grind [twoLevelSimp.walker])
+  (twoLevelSimp.walker aig).walk aig (by grind [twoLevelSimp.walker]) |>.fst
 
 end Valaig.Transform
