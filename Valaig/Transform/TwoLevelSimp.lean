91f1a89449b5e9ced9dc7a289dccf20734b76825
George Rennie <georgerennie@gmail.com>
2026-06-12 17:40:22 +0100

    Proofs for Transform.TwoLevelSimp

--- Valaig/Transform/TwoLevelSimp.lean
+++ Valaig/Transform/TwoLevelSimp.lean
@@ -19,41 +19,49 @@ private def walker (old : WFAig) : old.CachingForwardsWalker WFAig Lit where
   stateMotive aig size le := ∀ {var : Var}, var.validIn old → var.validIn aig
   cacheMotive aig idx le sm var lt lit := lit.var.idx < idx
 
-  step var aig cache valid size sm cm  := Id.run do
+  step var aig cache valid size sm cm := Id.run do
     let (eq:=_) .and lhs rhs := aig[var] | return (aig, var)
 
     -- Map through cache to make sure we have optimized versions
     let lhs := cache.mapLit lhs
     let rhs := cache.mapLit rhs
 
-    match  _ : TwoLevelSimp.simplifyAnd lhs rhs (aig.asAnd lhs.var) (aig.asAnd rhs.var) with
-    | .lit lit => (aig.rewriteAnd var lit lit (lvalid := sorry) (rvalid := sorry), lit)
-    | .and l r => (aig.rewriteAnd var l r (lvalid := sorry) (rvalid := sorry), var)
+    match  heq : TwoLevelSimp.simplifyAnd lhs rhs (aig.asAnd lhs.var) (aig.asAnd rhs.var) with
+    | .lit lit =>
+      have := TwoLevelSimp.var_simplifyAnd (· < var) heq
+      (aig.rewriteAnd var lit lit, lit)
+    | .and l r =>
+      have := TwoLevelSimp.var_simplifyAnd (· < var) heq
+      (aig.rewriteAnd var l r , var)
 
   stepState := by
+    intro var
     intros
     simp only
     split
     · rw [Id.run, var_validIn];
       split
-      · rw [WFAig.raw_rewriteAnd, nodes_rewriteAnd]
-        · grind
-        · sorry
-        · sorry
-      · rw [WFAig.raw_rewriteAnd, nodes_rewriteAnd]
-        · grind
-        · sorry
-        · sorry
+      next heq =>
+        have := TwoLevelSimp.var_simplifyAnd (· ≠ var) heq
+        rw [WFAig.raw_rewriteAnd, nodes_rewriteAnd] <;> grind
+      next heq =>
+        have := TwoLevelSimp.var_simplifyAnd (· ≠ var) heq
+        rw [WFAig.raw_rewriteAnd, nodes_rewriteAnd] <;> grind
     · grind
 
   stepCache := by grind
   stepCacheNew := by
+    intro var
     intros
     simp only
     split
     · split
-      · rw [Id.run]; sorry
-      · rw [Id.run]; sorry
+      next heq =>
+        have := TwoLevelSimp.var_simplifyAnd (·.idx < var.idx + 1) heq
+        simp only [Id.run]; grind
+      next heq =>
+        have := TwoLevelSimp.var_simplifyAnd (·.idx < var.idx + 1) heq
+        simp only [Id.run]; grind
     · grind
 
 end twoLevelSimp
