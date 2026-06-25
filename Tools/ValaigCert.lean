464061a286414f3e5d4525683e5379d2b4fc1378
George Rennie <georgerennie@gmail.com>
2026-06-25 22:36:23 +0100

    Print times

--- Tools/ValaigCert.lean
+++ Tools/ValaigCert.lean
@@ -13,6 +13,18 @@ open Valaig
 
 set_option warn.sorry false
 
+def println (s : String) : IO Unit := do
+  IO.println s
+  (←IO.getStdout).flush
+
+def time {α : Type} (s : String) (m : Unit -> IO α) : IO α := do
+  let start ← IO.monoNanosNow
+  let res ← m ()
+  let finish ← IO.monoNanosNow
+  let ms := (finish - start).toFloat / 1_000_000_000
+  IO.println s!"t {s}: {ms}"
+  return res
+
 def liftCoreM (action : Lean.CoreM α) : IO α := do
   let env ← Lean.mkEmptyEnvironment
   let ctx := { fileName := "", fileMap := default }
@@ -21,51 +33,52 @@ def liftCoreM (action : Lean.CoreM α) : IO α := do
 
 def checkUnsat (aig : WFAig) (lit : Lit) (reset : Bool) (valid : lit.validIn aig := by grind) : IO (Except String Unit) := do
   let res := Sat.toStd aig reset lit.var
-  IO.print s!"({res.fst.decls.size} nodes) "
+  println s!"{res.fst.decls.size} nodes "
   let entry : Std.Sat.AIG.Entrypoint Aig.LeafIdx := .mk res.fst (res.snd ⟨lit, valid⟩)
   -- let relabelled := entry.relabelNat
   -- let cnf := Std.Sat.AIG.toCNF relabelled
-  -- IO.print s!"({cnf.clauses.size} clauses, {cnf.numLiterals} literals) "
+  -- print s!"({cnf.clauses.size} clauses, {cnf.numLiterals} literals) "
   liftCoreM <| Sat.External.solveUnsatChecked entry
 
 def run (model cert : String) : IO Unit := do
-  IO.println "Reading model"
+  println "Reading model"
   let model ← IO.FS.Handle.mk model .read
   let (_, model) ← IO.ofExcept <| Valaig.Aiger.parse <| ← model.readBinToEnd
 
   let bad := model.bads[0]!.lit
 
-  IO.println "Reading certificate"
+  println "Reading certificate"
   let cert ← IO.FS.Handle.mk cert .read
   let (_, cert) ← IO.ofExcept <| Valaig.Aiger.parse <| ← cert.readBinToEnd
 
   -- let modelaig := Transform.twoLevelSimp (model.aig.toWF sorry)
   -- let model := { model with aig := modelaig }
 
-  IO.println "Constructing product circuit"
-  let (product, invbad) ← IO.ofExcept <| Valaig.Cert.appendCert model cert
+  println "Constructing product circuit"
+  let (product, invbad) ← time "product" <| fun _ => IO.ofExcept <| Valaig.Cert.appendCert model cert
 
-  IO.println s!"Product wellformed: {decide product.WF}"
+  if !product.WF then
+    IO.ofExcept <| .error "Product not wellformed!"
 
   -- Check that in a reset state the invariant holds
-  IO.print "Init: "
-  IO.ofExcept <| ← checkUnsat product invbad true sorry
-  IO.println "ok"
+  println "Init: "
+  IO.ofExcept <| ← time "init" <| fun _ => checkUnsat product invbad true sorry
+  println "ok"
 
   -- Check that whenever the invariant holds, the original property does too
   let (product, imp) := product.addAnd invbad.invert bad sorry sorry
-  IO.print "Implication: "
-  IO.ofExcept <| ← checkUnsat product imp false sorry
-  IO.println "ok"
+  println "Implication: "
+  IO.ofExcept <| ← time "implication" <| fun _ => checkUnsat product imp false sorry
+  println "ok"
 
   -- Check that the invariant is inductive
   let (product, map) := Transform.unroll product
   let (product, imp) := product.addAnd invbad.invert (map.mapLit invbad sorry) sorry sorry
-  IO.print "Consecution: "
-  IO.ofExcept <| ← checkUnsat product imp false sorry
-  IO.println "ok"
+  println "Consecution: "
+  IO.ofExcept <| ← time "consecution" <| fun _ => checkUnsat product imp false sorry
+  println "ok"
 
-  IO.println "s CERTIFICATE SAFE"
+  println "s CERTIFICATE SAFE"
 
   return ()
 
