f08c423e797be0ffb49e3d34b6a3b2375b1b8aa1
George Rennie <georgerennie@gmail.com>
2026-08-14 03:55:35 +0100

    semantics proofs for unroll

--- Valaig/Transform/Unroll.lean
+++ Valaig/Transform/Unroll.lean
@@ -8,25 +8,72 @@ open Aig
 
 namespace unroll
 
-@[always_inline]
-private def walker (old : WFAig) : old.CachingForwardsWalker WFAig Lit where
-  stateMotive aig size le := old ≤ aig
-  cacheMotive aig size le sm var lt lit := lit.validIn aig
+def assignMap (assign : LeafIdx -> Frame -> Bool) (map : Std.HashMap InputIdx InputIdx) : LeafIdx -> Frame -> Bool
+| .input idx, frame =>
+  match map.get? idx with
+  | some idx => assign idx 1
+  | none => assign idx frame
+| idx, frame => assign idx frame
 
-  init := old
+@[always_inline]
+private def walker (old : WFAig) : old.CachingForwardsWalker (WFAig × Std.HashMap InputIdx InputIdx) Lit where
+  stateMotive state size le := old ≤ state.fst ∧ ∀ idx, idx.validIn old → idx ∉ state.snd
+  cacheMotive state size le sm var lt lit :=
+    ∃ (h : lit.validIn state.fst),
+      ∀ {assign},
+        ⟦old, var, 1, assign⟧sv = ⟦state.fst, lit, 0, assignMap assign state.snd⟧s
+
+  init := ⟨old, .emptyWithCapacity old.numInputs⟩
   initState := by grind
 
-  step var aig cache valid size sm cm :=
-    match _ : aig[var] with
-    | .false       => (aig, .false)
-    | .and lhs rhs => let (eq:=_) (aig, var) := aig.addAnd (cache.mapLit lhs) (cache.mapLit rhs)
-                      (aig, var)
-    | .input _     => let (eq:=h) (aig, idx) := aig.addInput; (aig, idx.getVar aig)
-    | .latch idx   => (aig, idx.getNext aig)
+  step var state cache valid size sm cm :=
+    match _ : state.fst[var] with
+    | .false       => (state, .false)
+    | .and lhs rhs => let (eq:=_) (aig, var) := state.fst.addAnd (cache.mapLit lhs) (cache.mapLit rhs)
+                      ((aig, state.snd), var)
+    | .input idx   => let (eq:=h) (aig, idx') := state.fst.addInput;
+                      ((aig, state.snd.insert idx' idx), idx'.getVar aig)
+    | .latch idx   => (state, idx.getNext state.fst)
 
-  stepState := by intros; split <;> grind
-  stepCache := by intros; split <;> grind
-  stepCacheNew := by intros; split <;> grind
+  stepState var state := by
+    intros
+    constructor
+    · split <;> grind
+    · grind [mem_inputs_newInputIdx (aig := state.fst)]
+  stepCache var state cache valid _ _ sm cm var' hvar := by
+    intros
+    exists by intros; split <;> grind
+    intro assign
+    split
+    · simp; grind
+    · simp; grind
+    next idx =>
+      simp only [WFAig.raw_fst_addInput, WFAig.snd_addInput, snd_addInput]
+      rw [denoteS_mono (mono_addInput (aig := state.fst))]
+      · rcases cm hvar with ⟨_, cm⟩
+        rw [cm]
+        apply denoteS_of_assign_eq (wf := by grind)
+        grind [assignMap]
+      all_goals grind
+    · simp; grind
+  stepCacheNew var state cache valid _ sm cm := by
+    intros
+    exists by intros; split <;> grind
+    intro assign
+    split
+    · simp; grind
+    · simp; grind
+    · simp; grind [assignMap]
+    next idx heq =>
+      have : old.nodes[var] = Node.latch idx := by grind
+      simp only [denoteSV_getElem_nodes_latch this, getNext_eq]
+      have : state.fst.latches[idx] = old.latches[idx] := by grind
+      rw [denoteS_mono sm.left (lit := state.fst.latches[idx].next)]
+      · rw [Data.AbsMap.getElem_mono (mono_latches_mono sm.left)]
+        · apply denoteS_of_assign_eq (wf := by grind)
+          grind [assignMap]
+        · grind
+      all_goals grind
 
 end unroll
 
@@ -37,6 +84,7 @@ end unroll
   TODO: Strashing whilst unrolling
 -/
 def unroll (aig : WFAig) : WFAig × Data.VarCache Lit :=
-  (unroll.walker aig).walk
+  let res := (unroll.walker aig).walk
+  (res.fst.fst, res.snd)
 
 end Valaig.Transform
