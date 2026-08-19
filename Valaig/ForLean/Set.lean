a376bd8860e9d1d1b9007107e3c0dfd708371397
George Rennie <georgerennie@gmail.com>
2026-08-19 12:30:32 +0100

    simplify Set definition

--- Valaig/ForLean/Set.lean
+++ Valaig/ForLean/Set.lean
@@ -1,60 +1,21 @@
 module
 
-/-
-  Basic set definitions adapted from Mathlib.
--/
-
 @[expose] public section
 namespace Valaig
 variable {α : Type u}
 
+/--
+  Basic set definition as a predicate on a type.
+-/
 def Set (α : Type u) := α -> Prop
 
 namespace Set
 
-@[implicit_reducible]
-def ofPred {α : Type u} (p : α → Prop) : Set α :=
-  p
-
-@[implicit_reducible]
-protected def Mem (s : Set α) (a : α) : Prop :=
-  s a
-
-instance : Membership α (Set α) :=
-  ⟨Set.Mem⟩
+instance : Membership α (Set α) where
+  mem s a := s a
 
 theorem mem_iff {s : Set α} {a : α} :
     a ∈ s ↔ s a := by
   rfl
-
-@[ext, grind ext]
-theorem ext {a b : Set α} (h : ∀ (x : α), x ∈ a ↔ x ∈ b) : a = b :=
-  funext (fun x => propext (h x))
-
-instance : EmptyCollection (Set α) :=
-  ⟨fun _ => False⟩
-
-instance : Inhabited (Set α) :=
-  ⟨fun _ => False⟩
-
-protected def Subset (s₁ s₂ : Set α) :=
-  ∀ ⦃a⦄, a ∈ s₁ → a ∈ s₂
-
-instance : LE (Set α) := ⟨Set.Subset⟩
-
-protected def singleton (a : α) : Set α :=
-  fun x => x = a
-
-instance : Singleton α (Set α) := ⟨Set.singleton⟩
-
-protected def union (s1 s2 : Set α) : Set α :=
-  fun x => x ∈ s1 ∨ x ∈ s2
-
-instance : Union (Set α) := ⟨Set.union⟩
-
-protected def inter (s1 s2 : Set α) : Set α :=
-  fun x => x ∈ s1 ∧ x ∈ s2
-
-instance : Inter (Set α) := ⟨Set.inter⟩
 
 end Valaig.Set
