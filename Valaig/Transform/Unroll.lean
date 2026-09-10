ad4c88ae0a4be91eb34695e08b6cc39a1cfc9c41
George Rennie <georgerennie@gmail.com>
2026-09-10 18:08:33 +0100

    WIP: use Memo for Transform.Unroll

--- Valaig/Transform/Unroll.lean
+++ Valaig/Transform/Unroll.lean
@@ -1,6 +1,7 @@
 module
 
 public import Valaig.Aig
+public import Valaig.Data.Memo
 
 public section
 namespace Valaig.Transform
@@ -16,65 +17,149 @@ def assignMap (assign : LeafIdx -> Frame -> Bool) (map : Std.HashMap InputIdx In
   | none => assign idx frame
 | idx, frame => assign idx frame
 
+@[simp, grind unfold]
+abbrev walker.info (aig : WFAig) : Data.Memo.VisitorInfo (Var.In aig) Lit (WFAig × Std.HashMap InputIdx InputIdx) where
+  lt := (·.val < ·)
+  stateInv state := aig ≤ state.fst ∧ ∀ idx, idx.validIn aig → idx ∉ state.snd
+  cacheInv state sinv var lit :=
+  ∃ (h : lit.validIn state.fst),
+    ∀ {assign},
+      ⟦aig, var, assign⟧cv1 = ⟦state.fst, lit, assignMap assign state.snd⟧c0
+
+open Data.Memo.ActionM in
 @[always_inline]
-private def walker (old : WFAig) : old.CachingForwardsWalker (WFAig × Std.HashMap InputIdx InputIdx) Lit where
-  stateMotive state size le := old ≤ state.fst ∧ ∀ idx, idx.validIn old → idx ∉ state.snd
-  cacheMotive state size le sm var lt lit :=
-    ∃ (h : lit.validIn state.fst),
-      ∀ {assign},
-        ⟦old, var, assign⟧cv1 = ⟦state.fst, lit, assignMap assign state.snd⟧c0
+def walker (old : WFAig) : Data.Memo.Visitor (walker.info old) :=
+  fun state var => do
+    let map (lit : Lit) (valid : lit.var < var := by grind) :
+        Data.Memo.ActionM _ _ _ { l : Lit // l.validIn state.fst } := do
+      let new ← get ⟨lit.var, by grind⟩ valid
+      return ⟨lit.mapTo new, by grind⟩
 
-  init := ⟨old, .emptyWithCapacity old.numInputs⟩
-  initState := by grind
+    match _ : state.fst[var.val] with
+    | .false       => .just .false
+    | .and lhs rhs => let (aig, var) := state.fst.addAnd (←map lhs) (←map rhs)
+                      return ((aig, state.snd), var)
+    | .input idx   => let res := state.fst.addInput
+                      return ((res.fst, state.snd.insert res.snd idx), res.snd.getVar res.fst)
+    | .latch idx   => .just (idx.getNext state.fst)
 
-  step var state cache valid size sm cm :=
-    match _ : state.fst[var] with
-    | .false       => (state, .false)
-    | .and lhs rhs => let (eq:=_) (aig, var) := state.fst.addAnd (cache.mapLit lhs) (cache.mapLit rhs)
-                      ((aig, state.snd), var)
-    | .input idx   => let (eq:=h) (aig, idx') := state.fst.addInput;
-                      ((aig, state.snd.insert idx' idx), idx'.getVar aig)
-    | .latch idx   => (state, idx.getNext state.fst)
-
-  stepState var state := by
-    intros
-    constructor
-    · split <;> grind
-    · grind [mem_inputs_newInputIdx (aig := state.fst)]
-  stepCache var state cache valid _ _ sm cm var' hvar := by
-    intros
-    exists by intros; split <;> grind
-    intro assign
-    split
-    · simp; grind
-    · simp; grind
-    next idx =>
-      simp only [WFAig.raw_fst_addInput, WFAig.snd_addInput, snd_addInput]
+instance instWF {old : WFAig} : Data.Memo.WFVisitor (walker old) where
+  stateInv state root hsi query _ walk hci := by
+    apply walker.fun_cases_unfolding
+      (motive := fun a => ∀ h, (walker.info old).stateInv ((a walk hci).value?.get h).fst)
+    <;> simp only
+    <;> intros
+    <;> rename_i hpure
+    <;> revert hpure
+    <;> simp [-eq_self]
+    <;> grind [mem_inputs_newInputIdx (aig := state.fst)]
+  cacheInv state var hsi query _ walk hci := by
+    apply walker.fun_cases_unfolding
+      (motive := fun a => ∀ h, (walker.info old).cacheInv ((a walk hci).value?.get h).fst sorry var ((a walk hci).value?.get h).snd)
+    <;> simp only
+    <;> intros
+    <;> rename_i hpure
+    <;> revert hpure
+    <;> simp [-eq_self]
+    · grind
+    · grind
+    · grind [assignMap]
+    · rename_i h
+      intro hpure
+      exists by grind only [!WFAig.is_WF, WF.mem_nodes_next, usr WF.NextsValid_of_WF]
+      simp only [WFAig.getElem_eq, getElem_eq] at h
+      simp only [WFAig.le_iff] at hsi
+      intro assign
+      have := @denoteCV_getElem_nodes_latch (h := h) (frame := 1) (wf := by grind) (assign := assignMap assign state.snd)
+      simp only [getNext_eq] at this
+      simp only [←this, denoteCV_eq]
+      rw [denoteC_mono hsi.left]
+      · apply denoteC_of_assign_eq (wf := by grind)
+        grind [assignMap]
+      · grind
+      · grind
+      · grind
+  cachePreservation state root var val hsi hci _ _ walk hci' := by
+    apply walker.fun_cases_unfolding
+      (motive := fun a => ∀ h, (walker.info old).cacheInv ((a walk hci').value?.get h).fst sorry var val)
+    <;> simp only
+    <;> intros
+    <;> rename_i hpure
+    <;> revert hpure
+    <;> simp [-eq_self]
+    · grind
+    · grind
+    · intro hpure
+      exists by grind
+      intro assign
       rw [denoteC_mono (mono_addInput (aig := state.fst))]
-      · rcases cm hvar with ⟨_, cm⟩
-        rw [cm]
+      · simp only [exists_prop] at hci
+        rw [hci.right]
         apply denoteC_of_assign_eq (wf := by grind)
         grind [assignMap]
       all_goals grind
-    · simp; grind
-  stepCacheNew var state cache valid _ sm cm := by
-    intros
-    exists by intros; split <;> grind
-    intro assign
-    split
-    · simp; grind
-    · simp; grind (splits := 100)
-    · simp; grind [assignMap]
-    next idx heq =>
-      have : old.nodes[var] = Node.latch idx := by grind
-      simp only [denoteCV_getElem_nodes_latch this, getNext_eq]
-      have : state.fst.latches[idx] = old.latches[idx] := by grind
-      rw [denoteC_mono sm.left (lit := state.fst.latches[idx].next)]
-      · rw [Data.AbsMap.getElem_mono (mono_latches_mono sm.left)]
-        · apply denoteC_of_assign_eq (wf := by grind)
-          grind [assignMap]
-        · grind
-      all_goals grind
+    · grind
+-- #exit
+
+-- @[always_inline]
+-- private def walker (old : WFAig) : old.CachingForwardsWalker (WFAig × Std.HashMap InputIdx InputIdx) Lit where
+--   stateMotive state size le := old ≤ state.fst ∧ ∀ idx, idx.validIn old → idx ∉ state.snd
+--   cacheMotive state size le sm var lt lit :=
+--     ∃ (h : lit.validIn state.fst),
+--       ∀ {assign},
+--         ⟦old, var, assign⟧cv1 = ⟦state.fst, lit, assignMap assign state.snd⟧c0
+
+--   init := ⟨old, .emptyWithCapacity old.numInputs⟩
+--   initState := by grind
+
+--   step var state cache valid size sm cm :=
+--     match _ : state.fst[var] with
+--     | .false       => (state, .false)
+--     | .and lhs rhs => let (eq:=_) (aig, var) := state.fst.addAnd (cache.mapLit lhs) (cache.mapLit rhs)
+--                       ((aig, state.snd), var)
+--     | .input idx   => let (eq:=h) (aig, idx') := state.fst.addInput;
+--                       ((aig, state.snd.insert idx' idx), idx'.getVar aig)
+--     | .latch idx   => (state, idx.getNext state.fst)
+
+--   stepState var state := by
+--     intros
+--     constructor
+--     · split <;> grind
+--     · grind [mem_inputs_newInputIdx (aig := state.fst)]
+--   stepCache var state cache valid _ _ sm cm var' hvar := by
+--     intros
+--     exists by intros; split <;> grind
+--     intro assign
+--     split
+--     · simp; grind
+--     · simp; grind
+--     next idx =>
+--       simp only [WFAig.raw_fst_addInput, WFAig.snd_addInput, snd_addInput]
+--       rw [denoteC_mono (mono_addInput (aig := state.fst))]
+--       · rcases cm hvar with ⟨_, cm⟩
+--         rw [cm]
+--         apply denoteC_of_assign_eq (wf := by grind)
+--         grind [assignMap]
+--       all_goals grind
+--     · simp; grind
+--   stepCacheNew var state cache valid _ sm cm := by
+--     intros
+--     exists by intros; split <;> grind
+--     intro assign
+--     split
+--     · simp; grind
+--     · simp; grind (splits := 100)
+--     · simp; grind [assignMap]
+--     next idx heq =>
+--       have : old.nodes[var] = Node.latch idx := by grind
+--       simp only [denoteCV_getElem_nodes_latch this, getNext_eq]
+--       have : state.fst.latches[idx] = old.latches[idx] := by grind
+--       rw [denoteC_mono sm.left (lit := state.fst.latches[idx].next)]
+--       · rw [Data.AbsMap.getElem_mono (mono_latches_mono sm.left)]
+--         · apply denoteC_of_assign_eq (wf := by grind)
+--           grind [assignMap]
+--         · grind
+--       all_goals grind
 
 end unroll
 
